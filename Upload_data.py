59668409e2e07197aa909e4b40da1c08f5363b8b
Nannoussa <onscherif9@gmail.com>
2016-10-01 00:22:35 +0100

    The uploading file is for uploading and reading data

--- Upload_data.py
+++ Upload_data.py
@@ -1,37 +1,11 @@
 ###
-# Fichier: Upload&Extract_Data.py
+# Fichier:Upload_Data.py
 ####
-
-from pyexcel.cookbook import merge_all_to_a_book
-import glob
 import csv
 
-
-with open('dataset/MOS_Test.csv', "rb") as fsrce:
-    with open ('MOS_EXT.csv', "wb") as fdest:
-        reader=csv.reader(fsrce, delimiter=';')
-        writer = csv.writer(fdest,delimiter='|',lineterminator = "\n" )
- #       headers=[Rxlev,RXQ_sub,MOS]
-        for line in reader:
-            #print (row[1],row[8],row[15])
-            writer.writerow ([line[1],line[8],line[15]])
-            merge_all_to_a_book(glob.glob("MOS_EXT.csv"), "MOS_EXT.xls")
-fsrce.close()
-fdest.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#with open('dataset/MOS_Test.csv', "rb") as fsrce:
+fsrce=open('dataset/MOS_Test.csv', "rb")
+reader=csv.reader(fsrce, delimiter=';') #reader object which iterates over an input csv file(MOS_Test.csv)
 
 
 
